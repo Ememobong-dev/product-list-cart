2b3eab0aa14d112cf796e567272ca601dbddbae5
Olushola <ememobongumoren2@gmail.com>
2025-01-22 01:41:23 +0100

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,18 @@
 import { Col, Row } from "antd";
 import data from "@/app/data.json"
 import ProductCard from "@/components/ProductCard";
-
+import emptyCart from "../../public/assets/images/illustration-empty-cart.svg"
+import Image from "next/image";
 
 
 
 export default function Home() {
   return (
-    <div className="px-14 py-14">
-      <Row justify={'center'}>
-        <Col xs={16}>
+    <div className="p-16 ">
+      <Row justify={'center'} gutter={32}>
+        <Col xs={18}>
           <h2 className="text-rose-900 text-3xl font-bold">Desserts</h2>
-          <Row  justify={'center'} gutter={16}>
+          <Row className="py-10" justify={'center'} gutter={32}>
             {
               data.map( (item) => (
                 <Col key={item.name} lg={8}>
@@ -22,7 +23,15 @@ export default function Home() {
           </Row>
 
         </Col>
-        <Col xs={8}>
+        <Col xs={6}>
+          <div className="bg-white w-full p-6">
+            <h3 className="text-2xl text-red font-bold">Your Cart (0)</h3>
+            <span className="flex justify-center">
+              <Image width={200} height={200} src={emptyCart} alt="empty cart" />
+            </span>
+            <p className="text-center text-rose-900">Your added items will appear here</p>
+            
+          </div>
 
         </Col>
 
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -5,27 +5,28 @@ import cartIcon from "../../public/assets/images/icon-add-to-cart.svg";
 const ProductCard = ({ imageSrc, category, productName, productPrice  }: { imageSrc: string, category: string, productName: string, productPrice:number }) => {
   return (
     <div>
-      <div className="relative">
-        <span className="h-[200px] relative cover w-full">
-          <Image width={200} height={200}  src={imageSrc} className="cover w-full" alt="" />
-        </span>
-        <button className="absolute flex items-center bg-white border  border-red justify-center py-2 px-8">
-          <span>
-            <Image
-              src={cartIcon}
-              width={48}
-              height={48}
-              className="h-[200px] relative cover w-full"
-              alt=""
-            />
-          </span>
-          <span>Add to Cart</span>
-        </button>
+       <div className="relative">
+            <span className=" relative cover w-full">
+            <Image width={200} height={200}  src={imageSrc} className="object-cover rounded-xl w-full" alt={productName} />
+            </span>
+            <div className="flex items-center justify-center">
+                <button className="absolute shadow flex gap-3 items-center rounded-full bg-white border  border-red justify-center py-2 px-5">
+                <span>
+                    <Image
+                    src={cartIcon}
+                    width={24}
+                    height={24}
+                    alt="cart"
+                    />
+                </span>
+                <span className="text-rose-900 font-semibold">Add to Cart</span>
+                </button>
+            </div>
       </div>
-      <div>
-        <p> {category} </p>
-        <p> {productName} </p>
-        <p> {productPrice} </p>
+      <div className="relative font-semibold py-10">
+        <p className="text-rose-400"> {category} </p>
+        <p className="text-rose-900 text-xl"> {productName} </p>
+        <p className="text-red"> ${productPrice.toFixed(2)}</p>
       </div>
     </div>
   );
