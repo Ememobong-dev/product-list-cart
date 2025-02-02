d0b11292873ec0f453c99cf7b4b8ead60d29a6fd
Olushola <ememobongumoren2@gmail.com>
2025-02-02 16:52:34 +0100

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,132 +1,231 @@
 "use client";
 
-
 import { useEffect, useState } from "react";
-import { Col, Row } from "antd";
-import data from "@/app/data.json"
+import { Col, Modal, Row } from "antd";
+import data from "@/app/data.json";
 import ProductCard from "@/components/ProductCard";
-import emptyCart from "../../public/assets/images/illustration-empty-cart.svg"
-import cancelIcon from "../../public/assets/images/icon-remove-item.svg"
-import carbonIcon from "../../public/assets/images/icon-carbon-neutral.svg"
+import emptyCart from "../../public/assets/images/illustration-empty-cart.svg";
+import cancelIcon from "../../public/assets/images/icon-remove-item.svg";
+import carbonIcon from "../../public/assets/images/icon-carbon-neutral.svg";
+import orderConfirmedIcon from "../../public/assets/images/icon-order-confirmed.svg";
 import Image from "next/image";
 
-
-
-
-
-
-
 type CartListInterface = {
-  name: string,
-  price: number,
-  quantity: number
-}
+  name: string;
+  price: number;
+  quantity: number;
+  image: string;
+};
 
 export default function Home() {
   const [cart, setCart] = useState<CartListInterface[]>([]);
+  const [confirmOrder, setConfirmOrder] = useState(false);
 
-  useEffect ( () => {
-    setCart(JSON.parse(localStorage.getItem("cart") || "[]"))
-  }, [cart] )
+  useEffect(() => {
+    console.log("Got triggeredd heree");
+    // setCart([]);
+    const cartsValue: CartListInterface[] = JSON.parse(
+      localStorage.getItem("cart") || "[]"
+    );
+    setCart(cartsValue);
+  }, []);
 
-  const removeProductFromCart = (productName:string) => {
-    const updatedCart = cart.filter( (item) => item.name !== productName );
+  const removeProductFromCart = (productName: string) => {
+    const updatedCart = cart.filter((item) => item.name !== productName);
     setCart(updatedCart);
     localStorage.setItem("cart", JSON.stringify(updatedCart));
-  }
+  };
 
-  const totalPrice = cart.reduce( (accumulator, product) => {
-    return accumulator += product.price * product.quantity;
-  }, 0)
+  const totalPrice = cart.reduce((accumulator, product) => {
+    return (accumulator += product.price * product.quantity);
+  }, 0);
 
+  const handleOrderModal = () => {
+    setConfirmOrder(true);
+  };
 
-
-
-
-
+  const completeOrder = () => {
+    localStorage.removeItem("cart");
+    setConfirmOrder(false);
+    setCart([])
+  };
 
   return (
     <div className="md:p-16 p-5">
-      <Row justify={'center'} gutter={16} >
+      <Row justify={"center"} gutter={16}>
         <Col xs={24} md={16}>
           <h2 className="text-rose-900 text-3xl font-bold">Desserts</h2>
-          <Row className="py-10" justify={'center'} gutter={16}>
-            {
-              data.map( (item) => (
-                <Col key={item.name} lg={8}>
-                  <ProductCard category={item.category} productName={item.name} productPrice={item.price} imageSrc={item.image.desktop} />
-                </Col>
-              ) )
-            }
+          <Row className="py-10" justify={"center"} gutter={16}>
+            {data.map((item) => (
+              <Col key={item.name} lg={8}>
+                <ProductCard
+                  category={item.category}
+                  productName={item.name}
+                  productPrice={item.price}
+                  imageSrc={item.image.desktop}
+                  setCarts={setCart}
+                />
+              </Col>
+            ))}
           </Row>
-
         </Col>
         <Col xs={24} md={8}>
           <div className="bg-white w-full p-6">
             <h3 className="text-2xl text-red font-bold">
-              Your Cart ({cart.length })
+              Your Cart ({cart.length})
             </h3>
-            {
-              cart.length > 0 ? cart.map( (product) => (
-                <div key={product.name} >
+            {cart.length > 0 ? (
+              cart.map((product) => (
+                <div key={product.name}>
                   <div className=" w-full my-5 flex justify-between items-center">
                     <div>
                       <p className="font-bold">{product.name}</p>
                       <div className="flex gap-5 items-center">
-                        <span className="text-red font-bold"> {product.quantity}x </span>
-                        <span className="text-rose-500"> @${product.price.toFixed(2)} </span>
-                        <span className=" text-rose-300 font-bold"> @${(product.quantity * product.price).toFixed(2)} </span>
+                        <span className="text-red font-bold">
+                          {" "}
+                          {product.quantity}x{" "}
+                        </span>
+                        <span className="text-rose-500">
+                          {" "}
+                          @${product.price.toFixed(2)}{" "}
+                        </span>
+                        <span className=" text-rose-300 font-bold">
+                          {" "}
+                          @${(product.quantity * product.price).toFixed(2)}{" "}
+                        </span>
                       </div>
                     </div>
                     <button onClick={() => removeProductFromCart(product.name)}>
-                      <Image 
+                      <Image
                         src={cancelIcon}
-                        className="object-cover rounded-full border border-rose-400 hover:border-black" 
-                        width={16} 
-                        alt="cancel icon" 
+                        className="object-cover rounded-full border border-rose-400 hover:border-black"
+                        width={16}
+                        alt="cancel icon"
                       />
                     </button>
                   </div>
                   <hr />
                 </div>
-              ) ) 
-              :
-              (
-                <>
+              ))
+            ) : (
+              <>
                 <span className="flex justify-center">
-                  <Image width={200} height={200} src={emptyCart} alt="empty cart" />
+                  <Image
+                    width={200}
+                    height={200}
+                    src={emptyCart}
+                    alt="empty cart"
+                  />
                 </span>
-                <p className="text-center text-rose-900">Your added items will appear here</p>
-                </>
-              )
-            }
+                <p className="text-center text-rose-900">
+                  Your added items will appear here
+                </p>
+              </>
+            )}
 
-            {
-              cart.length > 0 && (
-                <>
+            {cart.length > 0 && (
+              <>
+                <div className="flex my-5 justify-between items-center">
+                  <p className="text-gray-800 ">Order Total</p>
+                  <p className="font-bold text-rose-900  text-3xl">
+                    ${totalPrice.toFixed(2)}
+                  </p>
+                </div>
+                <div className="flex gap-5 my-5 py-3 justify-center items-center px-2 bg-rose-100">
+                  <span>
+                    <Image src={carbonIcon} alt="" />
+                  </span>
+                  <p>
+                    This is a <span className="font-bold">carbon-neutral</span>{" "}
+                    delivery
+                  </p>
+                </div>
+                <button
+                  onClick={handleOrderModal}
+                  className="py-3 px-8 w-full bg-red hover:bg-[#722d19] text-white rounded-3xl"
+                >
+                  Confirm Order
+                </button>
+              </>
+            )}
+
+            <Modal
+              footer={null}
+              onCancel={() => setConfirmOrder(false)}
+              open={confirmOrder}
+            >
+              <div className="p-5">
+                <span>
+                  <Image
+                    src={orderConfirmedIcon}
+                    width={28}
+                    height={28}
+                    alt="order confirmed"
+                  />
+                </span>
+                <h2>Order Confirmed</h2>
+                <p>We hope you enjoy your food!</p>
+
+                {/* Product list */}
+
+                <div className="py-2 px-4 my-5 bg-rose-100">
+                  {cart.length > 0 &&
+                    cart.map((product) => (
+                      <div key={product.name}>
+                        <div className=" w-full my-3 flex justify-between items-center">
+                          <div className="flex items-center gap-4">
+                            <span>
+                              <Image
+                                className="object-cover rounded-xl"
+                                src={product.image}
+                                width={48}
+                                height={48}
+                                alt={product.name}
+                              />
+                            </span>
+                            <div>
+                              <p className="font-bold">{product.name}</p>
+                              <div className="flex gap-5 items-center">
+                                <span className="text-red font-bold">
+                                  {" "}
+                                  {product.quantity}x{" "}
+                                </span>
+                                <span className="text-rose-500">
+                                  {" "}
+                                  @${product.price.toFixed(2)}{" "}
+                                </span>
+                              </div>
+                            </div>
+                          </div>
+
+                          <span className=" text-rose-900 font-bold">
+                            {" "}
+                            ${(product.quantity * product.price).toFixed(
+                              2
+                            )}{" "}
+                          </span>
+                        </div>
+                        <hr />
+                      </div>
+                    ))}
                   <div className="flex my-5 justify-between items-center">
                     <p className="text-gray-800 ">Order Total</p>
-                    <p className="font-bold text-3xl">${totalPrice.toFixed(2)}</p>
+                    <p className="font-bold text-rose-900 text-2xl">
+                      ${totalPrice.toFixed(2)}
+                    </p>
                   </div>
-                  <div className="flex gap-5 my-5 py-3 justify-center items-center px-2 bg-rose-100">
-                    <span>
-                      <Image src={carbonIcon} alt="" />
-                    </span>
-                    <p>This is a <span className="font-bold">carbon-neutral</span> delivery</p>
-                  </div>
-                  <button className="py-3 px-8 w-full bg-red hover:bg-[#722d19] text-white rounded-3xl">Confirm Order</button>
-                </>
-              )
-            }
-            
-            
+                </div>
+                <button
+                  onClick={completeOrder}
+                  className="py-3 px-8 w-full bg-red hover:bg-[#722d19] text-white rounded-3xl"
+                >
+                  Start New Order
+                </button>
+              </div>
+            </Modal>
           </div>
-
         </Col>
-
-
       </Row>
-
     </div>
   );
 }
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -3,135 +3,152 @@
 import Image from "next/image";
 import React, { useState } from "react";
 import cartIcon from "../../public/assets/images/icon-add-to-cart.svg";
-import increaseIcon from '../../public/assets/images/icon-increment-quantity.svg';
-import decreaseIcon from '../../public/assets/images/icon-decrement-quantity.svg';
-
+import increaseIcon from "../../public/assets/images/icon-increment-quantity.svg";
+import decreaseIcon from "../../public/assets/images/icon-decrement-quantity.svg";
 
 type CartListInterface = {
-    name: string,
-    price: number,
-    quantity: number
-}
+  name: string;
+  price: number;
+  quantity: number;
+  image: string;
+};
 
+const ProductCard = ({
+  imageSrc,
+  category,
+  productName,
+  productPrice,
+  setCarts,
+}: {
+  imageSrc: string;
+  category: string;
+  productName: string;
+  productPrice: number;
+  setCarts: (val: CartListInterface[]) => void;
+}) => {
+  const [showCounter, setShowCounter] = useState<boolean>(false);
+  const [count, setCount] = useState<number>(1);
 
+  const handleIncrease = () => {
+    const newCount = count + 1;
+    setCount(newCount);
+    updateQuantity(newCount);
+  };
 
+  const handleDecrease = () => {
+    const newCount = count - 1;
+    if (newCount > 0) {
+      updateQuantity(newCount);
+      setCount(newCount);
+    }
+  };
 
-const ProductCard = ({ imageSrc, category, productName, productPrice  }: { imageSrc: string, category: string, productName: string, productPrice:number }) => {
-    const [showCounter, setShowCounter] = useState<boolean>(false);
-    const [count, setCount ] = useState<number>(1)
+  const handleAddToCart = () => {
+    setShowCounter(true);
+    const cart: CartListInterface[] = JSON.parse(
+      localStorage.getItem("cart") || "[]"
+    );
 
+    const existingProduct = cart.findIndex((item) => item.name === productName);
 
-
-    const handleIncrease = () => {
-        setCount((prev) => {
-          const newCount = prev + 1;
-          updateQuantity(newCount);
-          return newCount;
-        });
-      };
-    
-      const handleDecrease = () => {
-        setCount((prev) => {
-          const newCount = prev > 1 ? prev - 1 : prev;
-          if (newCount !== prev) updateQuantity(newCount);
-          return newCount;
-        });
-      };
-    
-
-    const handleAddToCart = () => {
-        setShowCounter(true)
-
-        const cart : CartListInterface[] = JSON.parse(localStorage.getItem("cart") || "[]");
-
-        const existingProduct = cart.findIndex( (item) => item.name === productName );
-
-        if(existingProduct != -1){
-            cart[existingProduct].quantity += count
-        } else {
-            cart.push({
-                name: productName,
-                price: productPrice,
-                quantity: count,
-            })
-        }
-
-        localStorage.setItem('cart', JSON.stringify(cart) )
+    if (existingProduct !== -1) {
+      cart[existingProduct].quantity += count;
+    } else {
+      cart.push({
+        name: productName,
+        price: productPrice,
+        quantity: count,
+        image: imageSrc,
+      });
     }
 
-    const updateQuantity = (newCount : number) => {
-        const cart : CartListInterface[] = JSON.parse(localStorage.getItem("cart") || "[]");
+    setCarts(cart);
+    localStorage.setItem("cart", JSON.stringify(cart));
+  };
 
-        const existingProduct = cart.findIndex((item) => item.name === productName);
+  const updateQuantity = (newCount: number) => {
+    console.log(newCount, "New Count value");
+    const cart: CartListInterface[] = JSON.parse(
+      localStorage.getItem("cart") || "[]"
+    );
 
-        if (existingProduct !== -1) {
-            cart[existingProduct].quantity = newCount;
-            localStorage.setItem("cart", JSON.stringify(cart));
-        }
+    const existingProduct = cart.findIndex((item) => item.name === productName);
 
+    if (existingProduct !== -1) {
+      console.log("New count", newCount);
+      cart[existingProduct].quantity = newCount;
+      localStorage.setItem("cart", JSON.stringify(cart));
     }
-    
-
-
-
+    console.log(cart, "Cart value hereee");
+    setCarts(cart);
+  };
 
   return (
     <div>
-       <div className="relative">
-            <span className=" relative h-40">
-                <Image 
-                    width={200} 
-                    height={100}  
-                    src={imageSrc} 
-                    className="object-cover rounded-xl w-full" 
-                    alt={productName} 
-                />
-            </span>
-            <div className="flex items-center text-rose-900 hover:text-red cursor-pointer justify-center">
-                
-                <button 
-                    className={`absolute cursor-pointer shadow flex justify-between items-center gap-8 rounded-full  border  border-red py-2 px-8 ${showCounter ? "bg-red" : "bg-white"}`}
-                    onClick={handleAddToCart}
+      <div className="relative">
+        <span className=" relative h-40">
+          <Image
+            width={200}
+            height={100}
+            src={imageSrc}
+            className="object-cover rounded-xl w-full"
+            alt={productName}
+          />
+        </span>
+        <div className="flex items-center text-rose-900 hover:text-red cursor-pointer justify-center">
+          <button
+            className={`absolute cursor-pointer shadow flex justify-between items-center gap-8 rounded-full  border  border-red py-2 px-8 ${
+              showCounter ? "bg-red" : "bg-white"
+            }`}
+            onClick={handleAddToCart}
+          >
+            {showCounter ? (
+              <>
+                <span
+                  className="flex justify-center w-6 h-6 p-2 items-center rounded-full border hover:border-black border-rose-50"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    handleDecrease();
+                  }}
                 >
-                    {
-                        showCounter ? (
-                            <>
-                                <span className="flex justify-center w-6 h-6 p-2 items-center rounded-full border hover:border-black border-rose-50"
-                                onClick={handleDecrease}
-                                >
-                                    <Image src={decreaseIcon} className="rounded-full" alt="increase icon" height={12} width={12}  />
-                                </span>
+                  <Image
+                    src={decreaseIcon}
+                    className="rounded-full"
+                    alt="increase icon"
+                    height={12}
+                    width={12}
+                  />
+                </span>
 
-                                <span className="text-white">
-                                    {count}
-                                </span>
+                <span className="text-white">{count}</span>
 
-                                <span className="flex justify-center p-2 items-center rounded-full border border-rose-50 hover:border-black w-6 h-6" 
-                                onClick={handleIncrease}
-                                >
-                                    <Image src={increaseIcon} alt="increase icon" height={24} width={24} />
-                                </span>
-                            </>
-                        ) :
-                        (
-                            <>
-                            <span>
-                                <Image
-                                src={cartIcon}
-                                width={24}
-                                height={24}
-                                alt="cart"
-                                />
-                            </span>
-                            <span className="font-semibold">Add to Cart</span>
-                            </>
-                        )
-                    }
-                </button>
-
-            </div>
-       </div>
-       <div className="relative font-semibold py-10">
+                <span
+                  className="flex justify-center p-2 items-center rounded-full border border-rose-50 hover:border-black w-6 h-6"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    handleIncrease();
+                  }}
+                >
+                  <Image
+                    src={increaseIcon}
+                    alt="increase icon"
+                    height={24}
+                    width={24}
+                  />
+                </span>
+              </>
+            ) : (
+              <>
+                <span>
+                  <Image src={cartIcon} width={24} height={24} alt="cart" />
+                </span>
+                <span className="font-semibold">Add to Cart</span>
+              </>
+            )}
+          </button>
+        </div>
+      </div>
+      <div className="relative font-semibold py-10">
         <p className="text-rose-400"> {category} </p>
         <p className="text-rose-900"> {productName} </p>
         <p className="text-red"> ${productPrice.toFixed(2)}</p>
